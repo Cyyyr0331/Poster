3e763cddfb01af5a74cdb3cc8dabc3f46947e10d
Cyyyr0331 <yiranchen06@outlook.com>
2025-12-04 17:33:32 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'academic-blue': '#1e3a8a',
+                'academic-light': '#eff6ff',
+                'accent-orange': '#f97316',
+                'success-green': '#22c55e'
+            },
+            fontFamily: {
+                sans: ['Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+                serif: ['Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+            }
+        }
+    }
+}
